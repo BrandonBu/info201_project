3611ce6a42ddae108444ea85979791cb70e2515c
vivian <pwy8900@uw.edu>
2021-06-01 22:53:59 +0800

    intro

--- MusicRec/ui.R
+++ MusicRec/ui.R
@@ -1,33 +1,64 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
-library(shiny)
+library("dplyr")
+library("shiny")
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
+first_page <- tabPanel(
+    "Ethnic-Based Info ",
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            sliderInput(
+                inputId = "area_index",
+                label = "please adjust the area of county",
+                min = range(midwest$area)[1],
+                max = range(midwest$area)[2],
+                value = range(midwest$area)[1]
+            ),
+            radioButtons(
+                inputId = "state_index",
+                label = "please select a state",
+                choices = unique(midwest$state)
+            )
         ),
-
-        # Show a plot of the generated distribution
+        
         mainPanel(
-            plotOutput("distPlot")
+            h3("Average Percentage of Different Ethnic Groups Based on Area"),
+            tableOutput(outputId = "pop_first"),
+            h3("Population of Different Ethnic Groups Based on State"),
+            plotOutput(outputId = "pop_second")
         )
     )
-))
+)
+
+second_page <- tabPanel(
+    "Other Population Indices Info",
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput(
+                inputId = "area_ind",
+                label = "please adjust the area of county",
+                min = range(midwest$area)[1],
+                max = range(midwest$area)[2],
+                value = range(midwest$area)[1]
+            ),
+            radioButtons(
+                inputId = "state_ind",
+                label = "please select a state",
+                choices = unique(midwest$state)
+            )
+        ),
+        
+        mainPanel(
+            h3("App Introduction"),
+            img(src = "Logo.png", height = 80, width = 80),
+            p("This application "),
+            h3("Population of Different Indices Based on State"),
+            plotOutput(outputId = "pop_forth")
+        )
+    )
+)
+
+ui <- navbarPage(
+    "Music Recommendation",
+    first_page,
+    second_page
+)
