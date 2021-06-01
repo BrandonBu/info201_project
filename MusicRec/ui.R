8ac3388dc5b2fc810344b4563f772fb310431c11
vivian <pwy8900@uw.edu>
2021-06-01 23:03:59 +0800

    intro

--- MusicRec/ui.R
+++ MusicRec/ui.R
@@ -4,29 +4,12 @@ library("shiny")
 
 first_page <- tabPanel(
     "About us",
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput(
-                inputId = "area_index",
-                label = "please adjust the area of county",
-                min = range(midwest$area)[1],
-                max = range(midwest$area)[2],
-                value = range(midwest$area)[1]
-            ),
-            radioButtons(
-                inputId = "state_index",
-                label = "please select a state",
-                choices = unique(midwest$state)
-            )
-        ),
-        
         mainPanel(
             h3("App Introduction"),
             img(src = "Logo.png", height = 80, width = 80),
-            p("This application "),
+            p("This application ...."),
             plotOutput(outputId = "pop_second")
         )
-    )
 )
 
 second_page <- tabPanel(
